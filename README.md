b3ecb565a17c821b5d59c655bca25df03c4e4a31
Knightly <knightly.cavalier@gmail.com>
2023-01-28 08:39:04 -1000

    Initial work

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # knightlog
 
-Helpers for outputting information for MacroQuest
+Helpers for writing chat output in MacroQuest using spdlog.
 
-## Adding to your project
+## Quick(ish) Start
 
 Select a directory and add it:
 
@@ -13,12 +13,151 @@ git submodule add -b main https://github.com/Knightly1/knightlog.git contrib/kni
 
 Include in your plugin cpp:
 ```cpp
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#define SPDLOG_EOL ""
 #include <mq/Plugin.h>
 #include "contrib/knightlog/knightlog.h"
 ```
 
-Call one of the methods:
+Add to your globals:
 
 ```cpp
-knightlog::Info('This is a test');
+KnightLog* knightlog = nullptr;
+```
+
+Add to InitializePlugin()
+```cpp
+PLUGIN_API void InitializePlugin()
+{
+// ...snip...
+    knightlog = new KnightLog();
+```
+
+Add to your ShutdownPlugin()
+```cpp
+PLUGIN_API void ShutdownPlugin()
+{
+// ...snip...
+    delete knightlog;
+```
+
+Output something to chat:
+```cpp
+SPDLOG_INFO("Hello world.");
+```
+
+## Functions
+
+KnightLog is based around spdlog, so the available macros for output are:
+```cpp
+SPDLOG_TRACE("For trace messages.");
+SPDLOG_DEBUG("For debug messages.");
+SPDLOG_INFO("For informational messages.");
+SPDLOG_WARN("For warning messages.");
+SPDLOG_ERROR("For error messages.");
+SPDLOG_CRITICAL("For critical messages (also help messages).");
+```
+
+Keep in mind that the default logger only shows messages at the "info" level and above.  You can change this at runtime using the function
+`SetLogLevel()` -- for example, to turn on all messages at "trace" and above:
+
+```cpp
+knightlog->SetLogLevel("trace");
+```
+
+KnightLog doesn't allow you to turn the logging off, so the least information that you can see is by setting the logging level to critical which
+will only show critical messages.  For this reason, it's recommended that commands or outputs (like help) that MUST always provide feedback be output
+with SPDLOG_CRITICAL.  Otherwise, when your logging level changes you will lose that output.
+
+You can also get the current log level:
+```cpp
+std::string currentLogLevel = knightlog->GetLogLevel();
+```
+
+And you can get the color codes for each of the colors by passing in their enum:
+```cpp
+std::string colorCodeForWarn = knighttlog->GetColorByLevel(spdlog::level::warn);
+```
+
+The other functions (Setting Patterns and colors) are described below.
+
+## Changing log settings
+
+KnightLog allows you to change the log format and the colors of each log level as well as set the log level of your program (for what shows).
+
+### Log Format
+
+The default log format is `"%^[%T] %L [%n] :: %v%$"` the deciphering of which can be found here: https://github.com/gabime/spdlog/wiki/3.-Custom-formatting#pattern-flags
+
+There is a deviation from the docs in that `%^` and `%$` in the docs are said to only be usable once.  These are overridden here so you can
+use them as many times as you'd like.  `%^` is just the color code for the logging level that you are at, while `%$` is equivalent to `\ax`.
+
+There are also two additional options:
+```
+%j - The source file of the currently running macro (if any)
+%k - The line of the currently running macro (if any)
+%q - Equivalent to "(%j :: Line %k)" (if a macro is running)
+```
+
+KnightLog's constructor takes an argument of the logging format you'd prefer, but the pattern can also be changed at any time at runtime.
+
+For example, if you wanted to match MQ2Nav's log settings you would call:
+```cpp
+PLUGIN_API void InitializePlugin()
+{
+// ...snip...
+    knightlog = new KnightLog("%^[%n]%$ %v");
+```
+
+If you later wanted to change the pattern (perhaps for a specific event) you would call SetPattern.  For example, you can hardcode colors as well if you want all of
+your messages to look the same, using the MQ color codes.  To do that at a point in time:
+
+```cpp
+knightlog->SetPattern("\ay[%n]\ax %v");
+```
+
+### Setting Colors
+
+Colors for each of the levels can be set at runtime by passing in either a map or updating a single color.  If you are updating more than one color, the most efficient
+way is to pass in a map.  Either way, it is done by passing in the enum for the level you want to set and the color you want to use.  Abbreviations can be used for colors
+(for example `r` for red) or the full hex code can be used in the form of `#xxxxxx`.  For example, to set all the colors:
+
+```cpp
+PLUGIN_API void InitializePlugin()
+{
+// ...snip...
+    knightlog = new KnightLog();
+    knightlog->SetColorByLevel(
+        {
+            {spdlog::level::trace,    "#FF00FF"},
+            {spdlog::level::debug,    "#FF8C00"},
+            {spdlog::level::info,     "#FFFFFF"},
+            {spdlog::level::warn,     "#FFD700"},
+            {spdlog::level::err,      "#F22613"},
+            {spdlog::level::critical, "#F22613"}
+        }
+    );
+```
+
+Or to just set debug and critical:
+```cpp
+PLUGIN_API void InitializePlugin()
+{
+// ...snip...
+    knightlog = new KnightLog();
+    knightlog->SetColorByLevel(
+        {
+            {spdlog::level::debug,    "#FF8C00"},
+            {spdlog::level::critical, "#F22613"}
+        }
+    );
+```
+
+Or to just set info to white:
+```cpp
+PLUGIN_API void InitializePlugin()
+{
+// ...snip...
+    knightlog = new KnightLog();
+    knightlog->SetColorByLevel(spdlog::level::info, "w");
 ```
---
+++ knightlog.h
@@ -0,0 +1,288 @@
+#pragma once
+
+#include <spdlog/sinks/base_sink.h>
+class WriteChatSink : public spdlog::sinks::base_sink<spdlog::details::null_mutex>
+{
+protected:
+	void sink_it_(const spdlog::details::log_msg& msg) override
+	{
+		spdlog::memory_buf_t formatted;
+		spdlog::sinks::base_sink<spdlog::details::null_mutex>::formatter_->format(msg, formatted);
+		WriteChatf("%s", fmt::to_string(formatted).c_str());
+	}
+
+	void flush_() override {}
+};
+
+class KnightLog
+{
+private:
+	std::shared_ptr<spdlog::logger> m_logger = nullptr;
+	std::shared_ptr<WriteChatSink> m_chatSink = std::make_shared<WriteChatSink>();
+	static inline std::map<spdlog::level::level_enum, std::string> m_colorlevels =
+	{
+		{spdlog::level::trace,    "#FF00FF"},
+		{spdlog::level::debug,    "#FF8C00"},
+		{spdlog::level::info,     "#FFFFFF"},
+		{spdlog::level::warn,     "#FFD700"},
+		{spdlog::level::err,      "#F22613"},
+		{spdlog::level::critical, "#F22613"}
+	};
+	class flag_format_carat_colors;
+	class flag_format_dollar_colors;
+	class flag_format_j_macrosource;
+	class flag_format_k_macroline;
+	class flag_format_q_macrofull;
+
+	static inline std::string m_logformat = "%^[%T] %L [%n] :: %v%$";
+
+	void set_formatter(const char* logformat = m_logformat.data()) const
+	{
+		auto formatter = std::make_unique<spdlog::pattern_formatter>();
+		formatter->add_flag<flag_format_carat_colors>('^');
+		formatter->add_flag<flag_format_dollar_colors>('$');
+		formatter->set_pattern(logformat);
+		m_chatSink->set_formatter(std::move(formatter));
+	}
+
+	enum class MacroInfoLevels
+	{
+		Line,
+		SourceFile,
+		Full
+	};
+
+	static std::string get_macro_info(MacroInfoLevels infolevel)
+	{
+		std::string strReturn = "";
+		if (MQMacroBlockPtr pBlock = GetCurrentMacroBlock())
+		{
+			MQMacroLine& ml = pBlock->Line.at(gMacroBlock->CurrIndex);
+			switch (infolevel)
+			{
+			case MacroInfoLevels::Line:
+				strReturn = std::to_string(ml.LineNumber);
+				break;
+			case MacroInfoLevels::SourceFile:
+				strReturn = ml.SourceFile;
+				break;
+			case MacroInfoLevels::Full:
+				strReturn = fmt::format("({} :: Line {})", ml.SourceFile, ml.LineNumber);
+				break;
+			}
+		}
+		return strReturn;
+	}
+public:
+	KnightLog(const char* logformat = m_logformat.data())
+	{
+		m_logformat = logformat;
+		m_logger = std::make_shared<spdlog::logger>(mqplugin::PluginName);
+		m_logger->set_level(spdlog::level::info);
+		m_logger->flush_on(spdlog::level::info);
+
+		m_chatSink->set_level(spdlog::level::trace);
+
+		set_formatter(logformat);
+
+		m_logger->sinks().push_back(m_chatSink);
+
+		spdlog::set_default_logger(m_logger);	
+	}
+
+	~KnightLog()
+	{
+		spdlog::shutdown();		
+	}
+
+	void SetPattern(const char* pattern) const
+	{
+		m_logformat = pattern;
+		set_formatter(pattern);
+	}
+
+	std::string GetLogLevel() const
+	{
+		switch (m_logger->level())
+		{
+		case spdlog::level::trace:
+			return "trace";
+		case spdlog::level::debug:
+			return "debug";
+		case spdlog::level::info:
+			return "info";
+		case spdlog::level::warn:
+			return "warn";
+		case spdlog::level::err:
+			return "error";
+		case spdlog::level::critical:
+			return "critical";
+		case spdlog::level::off:
+			return "off";
+		default:
+			return std::to_string(m_logger->level());
+		}
+	}
+
+	static std::string GetColorByLevel(spdlog::level::level_enum i)
+	{
+		if(!m_colorlevels.empty() && (m_colorlevels.find(i) != m_colorlevels.end()))
+			return m_colorlevels[i];
+
+		return "#000000";
+	}
+
+	bool SetColorByLevel(const std::map<spdlog::level::level_enum, std::string>& colormap) const
+	{
+		if (!colormap.empty())
+		{
+			bool success_state = true;
+			for (const auto& it : colormap)
+			{
+				if(!SetColorByLevel(it.first, it.second, false))
+				{
+					success_state = false;
+				}
+			}
+			set_formatter();
+			return success_state;
+		}
+
+		return false;
+	}
+
+	bool SetColorByLevel(spdlog::level::level_enum i, const std::string& color, bool recycle = true) const
+	{
+		if(!m_colorlevels.empty() && m_colorlevels.find(i) != m_colorlevels.end())
+		{
+			const int length = color.size();
+			// Valid colors are single letter, 3 letter, or 6 digit hex
+			if (length == 1 || length == 3 || length == 7)
+			{
+				m_colorlevels[i] = color;
+				if (recycle)
+				{
+					set_formatter();
+				}
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool SetLogLevel(std::string_view level) const
+	{
+		// off is intentionally excluded
+		if (ci_equals(level, "trace"))
+		{
+			m_logger->set_level(spdlog::level::trace);
+		}
+		else if (ci_equals(level, "debug"))
+		{
+			m_logger->set_level(spdlog::level::debug);
+		}
+		else if (ci_equals(level, "info"))
+		{
+			m_logger->set_level(spdlog::level::info);
+		}
+		else if (ci_equals(level, "warn") || ci_equals(level, "warning"))
+		{
+			m_logger->set_level(spdlog::level::warn);
+		}
+		else if (ci_equals(level, "err") || ci_equals(level, "error"))
+		{
+			m_logger->set_level(spdlog::level::err);
+		}
+		else if (ci_equals(level, "critical") || ci_equals(level, "fatal"))
+		{
+			m_logger->set_level(spdlog::level::critical);
+		}
+		else
+		{
+			return false;
+		}
+		m_logger->flush_on(m_logger->level());
+		return true;
+	}
+};
+extern KnightLog* knightlog;
+
+class KnightLog::flag_format_carat_colors: public spdlog::custom_flag_formatter
+{
+public:
+    void format(const spdlog::details::log_msg& msg, const std::tm&, spdlog::memory_buf_t& dest) override
+    {
+		std::string color_code = "\a";
+
+		color_code += GetColorByLevel(msg.level);
+
+    	dest.append(color_code.data(), color_code.data() + color_code.size());
+    }
+
+    std::unique_ptr<custom_flag_formatter> clone() const override
+    {
+        return spdlog::details::make_unique<flag_format_carat_colors>();
+    }
+};
+
+class KnightLog::flag_format_dollar_colors: public spdlog::custom_flag_formatter
+{
+public:
+    void format(const spdlog::details::log_msg&, const std::tm&, spdlog::memory_buf_t& dest) override
+    {
+		std::string color_code = "\ax";
+        dest.append(color_code.data(), color_code.data() + color_code.size());
+    }
+
+    std::unique_ptr<custom_flag_formatter> clone() const override
+    {
+        return spdlog::details::make_unique<flag_format_dollar_colors>();
+    }
+};
+
+class KnightLog::flag_format_j_macrosource: public spdlog::custom_flag_formatter
+{
+public:
+    void format(const spdlog::details::log_msg& msg, const std::tm&, spdlog::memory_buf_t& dest) override
+    {
+		std::string out = get_macro_info(MacroInfoLevels::SourceFile);
+    	dest.append(out.data(), out.data() + out.size());
+    }
+
+    std::unique_ptr<custom_flag_formatter> clone() const override
+    {
+        return spdlog::details::make_unique<flag_format_j_macrosource>();
+    }
+};
+
+class KnightLog::flag_format_k_macroline: public spdlog::custom_flag_formatter
+{
+public:
+    void format(const spdlog::details::log_msg& msg, const std::tm&, spdlog::memory_buf_t& dest) override
+    {
+		std::string out = get_macro_info(MacroInfoLevels::Line);
+    	dest.append(out.data(), out.data() + out.size());
+    }
+
+    std::unique_ptr<custom_flag_formatter> clone() const override
+    {
+        return spdlog::details::make_unique<flag_format_k_macroline>();
+    }
+};
+
+class KnightLog::flag_format_q_macrofull: public spdlog::custom_flag_formatter
+{
+public:
+    void format(const spdlog::details::log_msg& msg, const std::tm&, spdlog::memory_buf_t& dest) override
+    {
+		std::string out = get_macro_info(MacroInfoLevels::Full);
+    	dest.append(out.data(), out.data() + out.size());
+    }
+
+    std::unique_ptr<custom_flag_formatter> clone() const override
+    {
+        return spdlog::details::make_unique<flag_format_q_macrofull>();
+    }
+};
+
